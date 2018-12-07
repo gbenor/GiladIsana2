f180a399ccadf3a8ac20b2f6bf410fd8f3007b71
gbenor <benorgi@post.bgu.ac.il>
2018-12-07 17:41:27 +0200

    all must supply hint

--- Code/Utils.py
+++ Code/Utils.py
@@ -7,7 +7,7 @@ class TooManyMatches(Exception):
     pass
 
 
-def extract_site (site_with_stars_and_hashtags, mRNA_seq_extended, mRNA_seq_extended_offset, full_mrna_seq, ensg, vienna_hint=None):
+def extract_site (site_with_stars_and_hashtags, mRNA_seq_extended, mRNA_seq_extended_offset, full_mrna_seq, ensg, hint):
     site_stars = site_with_stars_and_hashtags.count("*")
     site_hashtags = site_with_stars_and_hashtags.count("#")
     site = site_with_stars_and_hashtags.replace("*", "")
@@ -21,27 +21,13 @@ def extract_site (site_with_stars_and_hashtags, mRNA_seq_extended, mRNA_seq_exte
             "no match for the site within the full mRNA seq. \nENSG={}\nsite={}\nmrna={}".format(ensg, site,
                                                                                                  full_mrna_seq))
     if len(start_end_list) > 1:
-        if not vienna_hint:
-            raise TooManyMatches(
-                "too many matches for the site within the full mRNA seq. \nENSG={}\nsite={}\nlist={}\nvienna_hint={}".format(ensg, site,
-                                                                                                        start_end_list, vienna_hint))
 
-        dist = [distance.euclidean(vienna_hint, x) for x in start_end_list]
+        dist = [distance.euclidean(hint, x) for x in start_end_list]
         assert min(dist)<50, "I think i found incorrect site place\nENSG={}\nsite={}\nlist={}\nvienna_hint={}".format(ensg, site,
-                                                                                                        start_end_list, vienna_hint)
+                                                                                                                      start_end_list, hint)
 
         site_start_loc, site_end_loc = start_end_list[np.argmin(dist)]
         one_match = False
-        # for p in start_end_list:
-        #     s, e = p
-        #     sv, ev = vienna_hint
-        #     if s>= sv and e<=ev:
-        #         site_start_loc, site_end_loc = p
-        #         one_match = False
-        # if one_match:
-        #     raise TooManyMatches(
-        #         "try to handle with vienna_hint. unfortuntly, too many matches for the site within the full mRNA seq. \nENSG={}\nsite={}\nlist={}\nvienna_hint={}".format(ensg, site,
-        #                                                                                                  start_end_list, vienna_hint))
 
 
     if one_match:

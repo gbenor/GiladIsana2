062fc3546c437e58bfd658f25f70a585a10554c7
gbenor <benorgi@post.bgu.ac.il>
2019-01-21 23:18:55 +0200

    stable version

---
+++ Code/Datafiles_Prepare/mirBaseUtils.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from Bio import SeqIO
+
+def read_mirbase_file(mirbase_file):
+    fasta_sequences = SeqIO.parse(open(mirbase_file), 'fasta')
+    miRBase_seq_list = []
+    miRBase_dic = {}
+    for fasta in fasta_sequences:
+        mi_name, ma_name, mi_seq = fasta.id, fasta.description.split()[1], str(fasta.seq)
+       # if mi_name.startswith('hsa'):
+        if (True):
+            miRBase_dic[ma_name] = [mi_name, mi_seq]
+            miRBase_seq_list.append(mi_seq)
+    print ('number of entrences in miRBase: ', len(miRBase_dic))
+    return miRBase_dic
+
+
+def insert_mirna(in_df, mirbase_file, col_name):
+    def find_mrna(row):
+        mrna_id = row[col_name]
+        list_of_mrna_seq = [l for k, l in miRBase_dic.items() if l[0] == mrna_id]
+        try:
+            return list_of_mrna_seq[0][1]
+        except IndexError:
+            return "No mrna match!!!"
+
+    miRBase_dic = read_mirbase_file(mirbase_file)
+    in_df['miRNA_seq'] = in_df.apply(find_mrna, axis=1)
+    return in_df.copy()
+
+
